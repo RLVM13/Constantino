48b369df2ca4af1d14208786f4dd8353b7f68701
Rafa-Lasso <rlvm13@gmail.com>
2024-02-18 20:22:22 +0100

    Modificacion 1802

--- scripts.js
+++ scripts.js
@@ -1,43 +1,44 @@
 
 // GRUPO DE CLASE
+
 // Alberto González Romero
 // Gema Millán Casado
 // Rafael Lasso de la Vega
 
 const pregunta = {
-    titulo: '¿A quién ha doblado más veces Constantino Romero?',
-    respuestas: [
-        {
-            label: "Clint Eastwood",
-            id: "clint-eastwood",
-            name: "actor",
-            value: "clint-eastwood"
-        },
-        {
-            label: "James Earl",
-            id: "james-earl",
-            name: "actor",
-            value: "james-earl"
-        },
-        {
-            label: "Roger Moore",
-            id: "roger-moore",
-            name: "actor",
-            value: "roger-moore"
-        },
-        {
-            label: "William Shatner",
-            id: "william-shatner",
-            name: "actor",
-            value: "william-shatner"
-        },
-        {
-            label: "Arnold Schwarzenegger",
-            id: "arnold-schwarzenegger",
-            name: "actor",
-            value: "arnold-schwarzenegger"
-        }
-    ]
+  titulo: '¿A quién ha doblado más veces Constantino Romero?',
+  respuestas: [
+    {
+      label: "Clint Eastwood",
+      id: "clint-eastwood",
+      name: "actor",
+      value: "clint-eastwood"
+    },
+    {
+      label: "James Earl",
+      id: "james-earl",
+      name: "actor",
+      value: "james-earl"
+    },
+    {
+      label: "Roger Moore",
+      id: "roger-moore",
+      name: "actor",
+      value: "roger-moore"
+    },
+    {
+      label: "William Shatner",
+      id: "william-shatner",
+      name: "actor",
+      value: "william-shatner"
+    },
+    {
+      label: "Arnold Schwarzenegger",
+      id: "arnold-schwarzenegger",
+      name: "actor",
+      value: "arnold-schwarzenegger"
+    }
+  ]
 }
 
 function imprimePregunta(pregunta) {
@@ -50,22 +51,33 @@ function imprimePregunta(pregunta) {
 }
 
 function imprimeTitulo(pregunta) {
-    return console.log("<p>" + pregunta.titulo + "</p>")
+  let titulo = "<p>" + pregunta.titulo + "</p>";
+  return titulo;
+}
+
+function imprimeTodasLasRespuestas(pregunta) {
+  let TodasRespuestas = "";
+  for (let respuesta = 0; respuesta < (pregunta.respuestas).length; respuesta++) {
+    TodasRespuesta += imprimeUnaRespuesta(respuesta);
   }
-  function imprimeTodasLasRespuestas() {
-    for (let respuesta = 0; respuesta < (pregunta.respuestas).length; respuesta++) {
-      imprimeUnaRespuesta(respuesta)
-    }
-  }
-  function imprimeUnaRespuesta(respuesta) {
-    imprimeLabel(respuesta)
-    imprimeInput(respuesta)
-  }
-  function imprimeLabel(respuesta) {
-    console.log(`<label for="${pregunta.respuestas[respuesta].id}">${pregunta.respuestas[respuesta].label}</label>`);
-  }
-  function imprimeInput(respuesta) {
-    console.log(`<input id="${pregunta.respuestas[respuesta].id}" name="${pregunta.respuestas[respuesta].name}" type="radio" value="${pregunta.respuestas[respuesta].value}">`)
-  }
+  return TodasRespuesta;
+}
+
+function imprimeUnaRespuesta(respuesta) {
+  let unaRespuesta = "";
+  aux += imprimeLabel(respuesta);
+  aux += imprimeInput(respuesta);
+  return unaRespuesta;
+}
+
+function imprimeLabel(respuesta) {
+  let label = `<label for="${respuesta.id}">${respuesta.label}</label>`;
+  return label;
+}
+
+function imprimeInput(respuesta) {
+  let input = `<input id="${respuesta.id}" name="${respuesta.name}" type="radio" value="${respuesta.value}">`;
+  return input;
+}
 
 document.getElementById("contenedorPreguntas").innerHTML = imprimePregunta(pregunta);
